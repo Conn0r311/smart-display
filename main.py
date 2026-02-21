1cdb838a0d094d2f2d16e0a2dbb236de4d71bf5f
Connor Crawford <concraw06@gmail.com>
2026-02-21 12:00:16 -0600

    Updated Smart Display Fullscreen ratio

--- main.py
+++ main.py
@@ -83,7 +83,8 @@ def get_todos():
 # =============================
 
 root = tk.Tk()
-root.attributes("-fullscreen", False)
+root.attributes("-fullscreen", True)
+root.overrideredirect(True)
 root.attributes("-alpha", 1.0)
 
 BG_COLOR = "#6EC6FF"
